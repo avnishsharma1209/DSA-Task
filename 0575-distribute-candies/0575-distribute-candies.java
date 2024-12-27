4279820e0c8a3d6f773c713aabb24a81d6b51b99
Avnish <169081112+avnishsharma1209@users.noreply.github.com>
2024-12-28 00:58:11 +0530

    Time: 30 ms (84.88%), Space: 45.9 MB (42.4%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -2,7 +2,7 @@ class Solution {
     public int distributeCandies(int[] candyType) {
         Set<Integer> set = new HashSet<>();
 
-        for (var i : candyType) 
+        for (var i : candyType)
             set.add(i);
         
         var n = candyType.length / 2;
