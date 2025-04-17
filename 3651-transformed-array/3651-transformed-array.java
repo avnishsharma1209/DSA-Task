3408d95697d775453c770ad3e253f0e4e85c176e
Avnish <169081112+avnishsharma1209@users.noreply.github.com>
2025-04-17 17:59:31 +0530

    Time: 1 ms (98.82%), Space: 45.3 MB (7.06%) - LeetHub

---
+++ 3651-transformed-array/3651-transformed-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int index = (i + nums[i]) % n;
+            if (index < 0) {
+                index += n;
+            }
+            result[i] = nums[index];
+        }
+        return result;
+    }
+}
