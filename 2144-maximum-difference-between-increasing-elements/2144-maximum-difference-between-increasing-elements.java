232779f07b09160b519da0f95f7cd5c092cfa290
Avnish <169081112+avnishsharma1209@users.noreply.github.com>
2025-06-16 08:38:49 +0530

    Time: 6 ms (8.89%), Space: 41.9 MB (73.85%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
@@ -3,10 +3,12 @@ class Solution {
         int a=-1;
         for(int i=nums.length-1;i>0;i--){
             for(int j=i-1;j>=0;j--){
-                a=Math.max(a,nums[i]-nums[j]);
+                if(nums[i]>nums[j]){
+                    a=Math.max(a,nums[i]-nums[j]);
+
+                }
+                // a=Math.max(a,nums[i]-nums[j]);
             }
-
-
         }
         if(a<=0){
             return -1;
@@ -16,3 +18,6 @@ class Solution {
         
     }
 }
+
+
+        
