d06ce8a173d0bcd00c7b7799d58f514d576542cb
Avnish <169081112+avnishsharma1209@users.noreply.github.com>
2025-06-17 12:03:39 +0530

    Time: 5 ms (57.43%), Space: 43.4 MB (66.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,11 +2,10 @@ public class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
 
-        int[] count = new int[26];  // For lowercase letters a-z
-
+        int[] count = new int[26]; 
         for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;  // Increase count for s
-            count[t.charAt(i) - 'a']--;  // Decrease count for t
+            count[s.charAt(i) - 'a']++; 
+            count[t.charAt(i) - 'a']--;  
         }
 
         for (int i : count) {
