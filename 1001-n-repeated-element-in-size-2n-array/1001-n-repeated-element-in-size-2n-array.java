8a38b3cdb35ef503f272f05a1cc4d07a410fd4b9
Avnish <169081112+avnishsharma1209@users.noreply.github.com>
2025-07-02 11:50:47 +0530

    Time: 7 ms (46.82%), Space: 45.1 MB (74.55%) - LeetHub

---
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+        return 1;
+    }
+}
