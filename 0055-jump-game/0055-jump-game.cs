0f1c8046590869cff7c83368602b2f0aeb56c4fd
Avnish <169081112+avnishsharma1209@users.noreply.github.com>
2025-09-17 23:50:14 +0530

    Time: 3 ms (25.56%), Space: 61.2 MB (37.18%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int power = 0;
+
+        for (int i = 0; i < nums.Length; i++) {
+            power --;
+            power = Math.Max(power, nums[i]);
+
+            if (power == 0 && i != nums.Length - 1)
+                return false;
+        }
+        return true;
+    }
+}
